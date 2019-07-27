f84100c3120c650f618737b12a220be656e74be5
sathishmepco <sathishmepco@gmail.com>
2019-07-27 23:33:40 +0530

    Tree Traversals - InOrder - PreOrder - PostOrder

--- src/com/java/datastructure/tree/TreeTraversal.java
+++ src/com/java/datastructure/tree/TreeTraversal.java
@@ -1,14 +1,32 @@
 package com.java.datastructure.tree;
 
 /*
- * This class has all the three types of Tree Traversals
- * 
+ * 	This class has all the three types of Tree Traversals
+	The below code will generate the binary tree like this
+ 	
+ 					40
+					
+			20				60
+			
+		10		30		50		70
+		
+	5
+	
+		8
+ 
+	InOrder Traversal
+	5 8 10 20 30 40 50 60 70 
+	PreOrder Traversal
+	40 20 10 5 8 30 60 50 70 
+	PostOrder Traversal
+	8 5 10 30 20 50 70 60 40 
+ 
  */
 public class TreeTraversal {
 	
 	public static void main(String[] args) {
 		TreeNode root = BinaryTree.createTree();
-		System.out.println("\nInOrder Traversal");
+		System.out.println("InOrder Traversal");
 		inorderTraversal(root);
 		
 		System.out.println("\nPreOrder Traversal");
